78ac24e0034a16ef6d047e1706d1f223aeb7997f
edmsvche <70912639+edmsvche@users.noreply.github.com>
2025-07-18 18:13:21 +0300

    Feature/cu 86c4efp7r/be chats issues
    * feat: fix BE chat issues
    
    * feat: fix session info

--- src/main/java/org/ua/drmp/dto/UserSessionResponse.java
+++ src/main/java/org/ua/drmp/dto/UserSessionResponse.java
@@ -2,5 +2,5 @@ package org.ua.drmp.dto;
 
 import org.ua.drmp.entity.DRMPRole;
 
-public record UserSessionResponse(long id, String email, String firstName, String lastName, DRMPRole role) {
+public record UserSessionResponse(long id, String email, String firstName, String lastName, DRMPRole role, Long companyId) {
 }
--- src/main/java/org/ua/drmp/service/impl/UserServiceImpl.java
+++ src/main/java/org/ua/drmp/service/impl/UserServiceImpl.java
@@ -123,8 +123,14 @@ public class UserServiceImpl implements UserService {
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
 		User user = userRepository.findByEmail(email)
 			.orElseThrow(() -> new UserNotFoundException("User not found"));
+		DRMPRole drmpRole = user.getRoles().stream().findFirst().orElseThrow().getName();
+		Long companyId = null;
+		if (drmpRole == DRMPRole.COMPANY_ADMIN || drmpRole == DRMPRole.COMPANY_USER) {
+			companyId = user.getCompanies().stream().findFirst().get().getId();
+		}
 		return new UserSessionResponse(
-			user.getId(), user.getEmail(), user.getFirstName(), user.getLastName(), user.getRoles().stream().findFirst().orElseThrow().getName()
+			user.getId(), user.getEmail(), user.getFirstName(), user.getLastName(), drmpRole, companyId
+
 		);
 	}
 
