1aed5f725af784a9da41f450ad96c1a5830b3457
edmsvche <voroshchuk721@icloud.com>
2025-07-21 18:04:56 +0300

    test: provide code for testing

--- src/main/java/org/ua/drmp/chat/ChatArchiverScheduler.java
+++ src/main/java/org/ua/drmp/chat/ChatArchiverScheduler.java
@@ -13,7 +13,8 @@ import org.ua.drmp.chat.repo.ChatRepository;
 public class ChatArchiverScheduler {
 	private final ChatRepository chatRepository;
 
-	@Scheduled(cron = "0 0 * * * *") // кожну годину
+	//@Scheduled(cron = "0 0 * * * *") // кожну годину
+	@Scheduled(cron = "0 */5 * * * *") // кожні 5 хвилин
 	public void archiveExpiredChats() {
 		List<Chat> expired = chatRepository.findByArchivedFalseAndExpiresAtBefore(Instant.now());
 		for (Chat chat : expired) {
