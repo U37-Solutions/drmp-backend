2ff67972132dfa6763f396e7aeae238a48099857
edmsvche <70912639+edmsvche@users.noreply.github.com>
2026-04-14 18:08:08 +0300

    fix: cfield delete with structure (#65)

--- src/main/java/org/ua/drmp/company/repo/CFieldValueRepository.java
+++ src/main/java/org/ua/drmp/company/repo/CFieldValueRepository.java
@@ -2,6 +2,7 @@ package org.ua.drmp.company.repo;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,4 +18,7 @@ public interface CFieldValueRepository extends JpaRepository<CFieldValue, Long> 
 	Optional<CFieldValue> findFirstByStructureIdAndValue(@Param("structureId") Long structureId,
 		@Param("value") String value);
 
+	@Modifying
+	@Query("delete from CFieldValue v where v.structure.id = :structureId")
+	void deleteByStructureId(@Param("structureId") Long structureId);
 }
