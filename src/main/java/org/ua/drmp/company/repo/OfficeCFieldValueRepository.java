c3ca376c8050ad7b536339eee3da9b29cfcdcc84
edmsvche <70912639+edmsvche@users.noreply.github.com>
2026-04-14 18:55:20 +0300

    fix: delete office relate to cfvalue (#66)

--- src/main/java/org/ua/drmp/company/repo/OfficeCFieldValueRepository.java
+++ src/main/java/org/ua/drmp/company/repo/OfficeCFieldValueRepository.java
@@ -2,6 +2,7 @@ package org.ua.drmp.company.repo;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,4 +18,15 @@ public interface OfficeCFieldValueRepository extends JpaRepository<OfficeCFieldV
 
 	@Query("SELECT COUNT(ocfv) FROM OfficeCFieldValue ocfv WHERE ocfv.value.id = :valueId")
 	long countByValueId(@Param("valueId") Long valueId);
+
+	@Modifying
+	@Query("""
+		delete from OfficeCFieldValue ocfv
+		where ocfv.value.id in (
+			select cv.id
+			from CFieldValue cv
+			where cv.structure.id = :structureId
+		)
+	""")
+	void deleteAllByStructureId(@Param("structureId") Long structureId);
 }
